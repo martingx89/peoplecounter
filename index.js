bb1c957eaa7f2cc465b4965ebe56f1eaa1ae538c
Marcin Adamiak <marcin_89@outlook.com>
2022-12-28 17:35:47 +0100

    Add save and reset

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // document.getElementById("count-el").innerText = 5;
 
 let count = 0;
+const saveEl = document.getElementById("save-el");
 const countEl = document.getElementById("count-el");
 
 function increment() {
 	count += 1;
 
-	countEl.innerText = count;
+	countEl.textContent = count;
+}
+
+function save() {
+	saveEl.textContent += count + " - ";
+	count = 0;
+	countEl.textContent = 0;
 }
